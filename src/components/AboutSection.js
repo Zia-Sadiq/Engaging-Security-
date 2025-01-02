5e70f04cdd38ed9b343d2a39f0deb192da30d46e
Zia-Sadiq <140486539+Zia-Sadiq@users.noreply.github.com>
2025-01-02 21:37:08 +0500

    AboutSection.js

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -12,7 +12,8 @@ const AboutSection = () => {
           </div>
           <div className="col-md-6 order-1 order-md-2">
             <h2 className="mb-4 fw-bold">About Us</h2>
-            <p><b>Need Engaging Static Security Guard Services?</b> Security became the most primary and like a basic need for any smooth running of business or any day to day business events or leisure get-together. This is also our main area of specialty to guard and secure, allowing you to relax and focus on your business life. We cover all major UK cities, whether you go South or North. We also promise to provide short-term contract site security on very short notice as a sub-contractor.</p>
+            <p><b>Need Engaging Static Security Guard Services?</b></p> 
+            <p> Security became the most primary and like a basic need for any smooth running of business or any day to day business events or leisure get-together. This is also our main area of specialty to guard and secure, allowing you to relax and focus on your business life. We cover all major UK cities, whether you go South or North. We also promise to provide short-term contract site security on very short notice as a sub-contractor.</p>
             <p>Our sites can be located in busy urban or remote rural areas. Potential threats like break-ins, vandalism, theft, and danger to your personnel or important business belongings are our primary concerns. Through years of experience in the security services industry, we adhere to high professional standards of customer satisfaction, reflected in our company slogan. We value our principles highly and work tirelessly round the clock to achieve our goals. Starting as a small entity, we have grown into a decent-sized company, enabling us to cater to businesses of all sizes and provide tailored service packages to meet their specific requirements.</p>
           </div>
         </div>
