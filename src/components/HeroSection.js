b9e1361749b5d03cf157f184bcc0099643f459a4
Zia-Sadiq <140486539+Zia-Sadiq@users.noreply.github.com>
2025-01-16 00:32:41 +0500

    HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './HeroSection.css';
-import heroImage from '../../src/assets/images/log.png'; // Import the image
+import heroImage from '../../src/assets/images/log.png';
+import phoneIcon from '../../src/assets/images/phone.png'; // Import phone icon image
 
 const HeroSection = () => {
   return (
@@ -9,26 +10,6 @@ const HeroSection = () => {
         <div className="row align-items-center">
           <div className="col-md-6 text-center text-md-start">
             <h1 className="display-4">
-              {/* <span className="letter">G</span>
-              <span className="letter">U</span>
-              <span className="letter">A</span>
-              <span className="letter">R</span>
-              <span className="letter">D</span>
-              <span className="letter"> </span>
-              <span className="letter">Y</span>
-              <span className="letter">o</span>
-              <span className="letter">u</span>
-              <span className="letter">r</span>
-              <span className="letter"> </span>
-              <span className="letter">W</span>
-              <span className="letter">O</span>
-              <span className="letter">R</span>
-              <span className="letter">K</span>
-              <span className="letter">P</span>
-              <span className="letter">L</span>
-              <span className="letter">A</span>
-              <span className="letter">C</span>
-              <span className="letter">E</span> */}
               <span className="letter">E</span>
               <span className="letter">N</span>
               <span className="letter">G</span>
@@ -46,17 +27,11 @@ const HeroSection = () => {
               <span className="letter">I</span>
               <span className="letter">T</span>
               <span className="letter">Y</span>
-              
-              
-
-
-
             </h1>
-            <p className="lead fw-bold" style={{ fontSize: '1.7rem' }}>Relax! You are Secure.</p> 
-
-            {/* <p className="lead fw-bold" style={{ fontSize: '1.7rem' }}>Engaging Security</p>  */}
-            {/* <p style={{ fontSize: '1.7rem' }}>Relax! You are Secure.</p>  */}
-            {/* <a href="#" className="btn btn-primary">Get Started</a> */}
+            <div className="d-flex align-items-center mt-3"> 
+              <img src={phoneIcon} alt="Phone Icon" className="me-2" width="50" height="50" /> 
+              <span><b>020 8087 1952</b></span> 
+            </div> 
           </div>
           <div className="col-md-6 text-center text-md-end">
             <img src={heroImage} alt="Hero Image" className="img-fluid" /> 
